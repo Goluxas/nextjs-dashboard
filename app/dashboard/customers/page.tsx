dcf08d8f2d38b58664ead86dabd0d733c85ee15a
Douglas Storz <douglasstorz@gmail.com>
2025-02-13 03:09:19 -0500

    Additional page title metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Customers (Unbuilt)'
+}
+
 export default function Page() {
     return <p>Dashboard &gt; Customers Page</p>;
 }
